ab3b59cfaa125a284e049da10d10ebe59d460791
j64545 <1974344286@qq.com>
2024-11-17 09:42:26 +0800

    Add files via upload

---
+++ Main.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+int gcd(int a, int b)
+{
+    int t;
+    while (b!= 0)
+    {
+        t = b;
+        b = a % b;
+        a = t;
+    }
+    return a;
+}
+int main()
+{
+    int a, b, x;
+    scanf("%d/%d", &a, &b);
+    x = gcd(a, b);
+    printf("%d/%d\n", a / x, b / x);
+    return 0;
+}
+
+
